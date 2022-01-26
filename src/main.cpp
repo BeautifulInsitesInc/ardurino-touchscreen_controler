2d95e4666ce5a64e53a847fd1c6929abcc31efd8
Savvy Insights <greg@savvyinsights.io>
2022-01-26 08:50:53 -0500

    Major cleanup, organization, and code rectification

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,30 @@
 #include <DallasTemperature.h>
 #include <LiquidCrystal_I2C.h>
 
-// ------- LCD DISPLAY ----------------------------
+// =================================================
+// ========== LCD DISPLAY ==========================
+// =================================================
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+// Display the splash screen
+void displaySplashscreen()
+{
+  lcd.init(); 
+  lcd.backlight();
+  lcd.setCursor(1,0);
+  lcd.print("CONCIERGE GROWERS");
+  lcd.setCursor(5,1);
+  lcd.print("Eat Good");
+  lcd.setCursor(4,2);
+  lcd.print("Feel Good");
+  lcd.setCursor(4,3);
+  lcd.print("Look Good");
+  delay(1000);
+  lcd.clear();
+}
+
+// Display the parts that don't change
 void displayMainscreenstatic()
 {
-  // Display the parts that don't change
   lcd.setCursor(0,0);
   lcd.print("Temp:");
   lcd.setCursor(1,1);
@@ -19,38 +38,29 @@ void displayMainscreenstatic()
   lcd.setCursor(14,3);
   lcd.print("(Time)");
 }
-// ------ TEMPURATURE SENSOR DS18B20 -----------------------
 
+void displayMainscreenData() // Display the data that changes on main screen
+{
+  lcd.setCursor(5,0);
+  // put if statement here to print c or f depending on settings
+  lcd.print(displayWaterTemp(waterThermometer));
+  lcd.print("C");
+  
+}
+
+// ------- END LCD DISPLAY ------------------------------
+
+// =======================================================
+// ======= TEMPURATURE SENSOR DS18B20 ====================
+// =======================================================
 #define ONE_WIRE_BUS 2 // Port the data wire is plugged into on the Arduino
 #define TEMPERATURE_PRECISION 11
-
 OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 DallasTemperature sensors(&oneWire); // Pass our oneWire reference to Dallas Temperature.
-
 DeviceAddress waterThermometer, airThermometer; // arrays to hold device addresses
 
-//  ----------- END TEMPURATURE SENSOR ---------------------
-void setup(void)
+void setupThermometers()
 {
-  // start serial port
-  Serial.begin(9600);
-  Serial.println("Starting Hydroponics Automation Controler");
-  // ---- Initallize LCD
-  lcd.init(); 
-  lcd.backlight();
-  lcd.setCursor(1,0);
-  lcd.print("CONCIERGE GROWERS");
-  lcd.setCursor(5,1);
-  lcd.print("Eat Good");
-   lcd.setCursor(4,2);
-  lcd.print("Feel Good");
-   lcd.setCursor(4,3);
-  lcd.print("Look Good");
-  delay(1000);
-  lcd.clear();
-  displayMainscreenstatic();
-  // ----- SETUP - THERMOMETER  -------
-  // Start up the library
   sensors.begin();
   // locate devices on the bus
   Serial.print("Locating devices...");
@@ -83,10 +93,8 @@ void setup(void)
   Serial.print("Device 1 Resolution: ");
   Serial.print(sensors.getResolution(airThermometer), DEC);
   Serial.println();
-  // ------------ END SETUP - THERMOMETER ------------
-
 }
-// ------- FUNCTIONS - THERMOMETER ----------------
+
 // function to print a device address
 void printAddress(DeviceAddress deviceAddress)
 {
@@ -98,39 +106,14 @@ void printAddress(DeviceAddress deviceAddress)
   }
 }
 
-// function to print the temperature for a device
-void printTemperature(DeviceAddress deviceAddress)
+// function to print information about a device
+void printData(DeviceAddress deviceAddress)
 {
-  float tempC = sensors.getTempC(deviceAddress);
-  if(tempC == DEVICE_DISCONNECTED_C) 
-  {
-    Serial.println("Error: Could not read temperature data");
-    return;
-  }
-  
-
-  /*// ---- Print to screen
-  lcd.setCursor(0,1);
-  lcd.print("                   ");
-  lcd.setCursor(1,2);
-  lcd.print("Water Tempurature");
-  lcd.setCursor(0,3);
-  lcd.print("                   ");
-  lcd.setCursor(2,3);
-  lcd.print("C:");
-  lcd.setCursor(4,3);
-  lcd.print(tempC);
-  lcd.setCursor(11,3);
-  lcd.print("F:");
-  lcd.setCursor(13,3);
-  lcd.print(DallasTemperature::toFahrenheit(tempC));
-   */
-  Serial.print("Temp C: ");
-  Serial.print(tempC);
-  Serial.print(" Temp F: ");
-  Serial.print(DallasTemperature::toFahrenheit(tempC));
-
-  
+  Serial.print("Device Address: ");
+  printAddress(deviceAddress);
+  Serial.print(" ");
+  printTemperature(deviceAddress);
+  Serial.println();
 }
 
 // function to print a device's resolution
@@ -140,45 +123,37 @@ void printResolution(DeviceAddress deviceAddress)
   Serial.print(sensors.getResolution(deviceAddress));
   Serial.println();
 }
-
-// main function to print information about a device
-void printData(DeviceAddress deviceAddress)
-{
-  Serial.print("Device Address: ");
-  printAddress(deviceAddress);
-  Serial.print(" ");
-  printTemperature(deviceAddress);
-  Serial.println();
-  //lcd.setCursor(1,0);
-  //lcd.print("CONCIERGE GROWERS");
-  lcd.setCursor(0,1);
-  lcd.print(" ");
-  }
-
+// function to return water tempurature
 float displayWaterTemp(DeviceAddress deviceAddress)
 {
   float tempC = sensors.getTempC(deviceAddress);
-
   if(tempC == DEVICE_DISCONNECTED_C) 
   {
     Serial.println("Error: Could not read temperature data");
     tempC = -10;
   }
   return tempC;
+  //Serial.print(DallasTemperature::toFahrenheit(tempC)); - to convert
 }
-// ------- END FUNCTIONS THERMOMETER -----------------------
-// ------- DISPLAY MAIN SCREEN --------------------------
-// --------- END DISPLAY MAIN SCREEN --------------------
 
-void displayMainscreenData() // Display the data that changes on main screen
+// ----------    END THERMOMETER  ------------
+
+// ==================================================
+// ===========  MAIN SETUP ==========================
+// ==================================================
+void setup(void)
 {
-  lcd.setCursor(5,0);
-  lcd.print(displayWaterTemp(waterThermometer));
-  lcd.print("C");
-  
-
-
+  Serial.begin(9600);// start serial port
+  Serial.println("Starting Hydroponics Automation Controler");
+  displaySplashscreen();
+  displayMainscreenstatic();
+  setupThermometers();
 }
+// ------- END MAIN SETUP ------------------
+
+// ====================================================
+// ===========  MAIN LOOP =============================
+// ====================================================
 
 void loop(void)
 {
@@ -188,12 +163,8 @@ void loop(void)
   sensors.requestTemperatures();
   Serial.println("DONE");
 
-  // print the device information
-  Serial.print("Water Tempurature :");
-  //-printData(waterThermometer);
-  //-printData(airThermometer);
-
   displayMainscreenData();
 
-  
 }
+
+// ----------------- END MAIN LOOP ------------------------
